464ecaea555e47073157aa4325af788ac3c79743
Svetlana4278Zh <svetlana.zh@inbox.ru>
2022-11-23 22:06:54 +0300

    Four tasks

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1
+        System.out.println("Задача 1");
+        for (int i=1; i<=10;i++){
+            System.out.println(i);
+        }
+        //Задача 2
+        System.out.println("\nЗадача 2");
+        for (int i=10; i>=1;i--){
+            System.out.println(i);
+        }
+        //Задача 3
+        System.out.println("\nЗадача 3");
+        for (int i=0; i<=17;i+=2){
+            System.out.println(i);
+        }
+        //Задача 4
+        System.out.println("\nЗадача 4");
+        for (int i=10; i>=-10;i--){
+            System.out.println(i);
+        }
+    }
+}
